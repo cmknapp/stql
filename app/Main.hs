afdf1d0388b44d9d5319752ec6319f68e23f1228
Cory Knapp <cory.m.knapp@gmail.com>
2020-04-26 10:40:06 +0100

    Basic repl

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,34 @@
 module Main where
 
-import Lib
+import qualified Data.Text.IO as T
+import System.IO
+import Data.Text (Text)
 
 main :: IO ()
-main = someFunc
+main = go
+
+go :: IO ()
+go = do
+    putStr "stql > "
+    hFlush stdout
+    input <- T.getLine
+    hFlush stdout
+    let result = process input
+    respond result
+    case result of 
+      Exit -> return ()
+      _ -> go
+
+
+data ReplResult = Exit | Continue | NoInput
+
+process :: Text -> ReplResult
+process input = case input of
+                  "" -> NoInput
+                  ".exit" -> Exit
+                  _ -> Continue
+
+respond :: ReplResult -> IO ()
+respond NoInput = return ()
+respond Exit = return ()
+respond Continue = T.putStrLn "Error reading input."
